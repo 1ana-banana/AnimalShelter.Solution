8eda11da3f90942e312af720c8f5baa3d4c9c060
Anastasia Han <anastasia.han@yahoo.com>
2022-04-24 13:05:58 -0400

    updated Program, Startup.cs

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -1,16 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using AnimalShelter.Models;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Reflection;
+using System.IO;
 
 namespace AnimalShelter
 {
@@ -27,11 +25,32 @@ namespace AnimalShelter
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddDbContext<AnimalShelterContext>(opt =>
+                opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+
+            // registered the Swagger generator, defining one or more Swagger documents.
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AnimalShelter", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo 
+            {
+                Title = "Animal Shelter Api",
+                Version = "v1", 
+                Description = "A .NET web API to organize animals at an animal shelter",
+                Contact = new OpenApiContact
+                {
+                Name = "Anastasia Han",
+                Url = new Uri("https://github.com/1ana-banana")
+                },
+                // License = new OpenApiLicense
+                // {
+                // Name = "Use under AFL",
+                // Url = new Uri("https://opensource.org/licenses/AFL-3.0"),
+                // }
             });
+        });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,11 +59,21 @@ namespace AnimalShelter
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AnimalShelter v1"));
+
+                // exposed the generated Swagger as JSON endpoint(s) using middleware
+                app.UseSwagger(c =>
+                {
+                    c.SerializeAsV2 = true;
+                });
+                    // inserted the swagger-ui middleware to expose interactive documentation, specifying the Swagger JSON endpoint(s) to power it from
+                    app.UseSwaggerUI(c => 
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "AnimalShelterApi v1");
+                    c.RoutePrefix = string.Empty;
+                });
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
